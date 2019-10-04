903a448c168833f517dbd4e1a0f6280342c170ae
Mario_95 <mrojasc95.mr@gmail.com>
2019-10-03 21:45:59 -0400

    user guess and random letter succesfully compred, global varibales an counters defined

--- assest/javascript/game.js
+++ assest/javascript/game.js
@@ -1,4 +1,24 @@
+// initialze counters wins and loses 
 
-// let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// let letter = onkeydown;
-// let letterPosition = alphabet.indexOf(letter)+1;
+let wins = 0
+let loses = 0
+let GL = 9
+let userLetter = ' ' 
+
+
+//alphabet array obatined from StackOverflow
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//randomly generate an index number 
+let randomIndex = Math.floor((Math.random() * 26));
+// pull a random letter fromt the array using a random index number 
+let randomLetter =  alphabet[randomIndex]
+
+console.log(randomIndex)
+console.log(randomLetter)
+
+for ( i = 0; i < 9 ; i++){
+
+    
+
+
+}
